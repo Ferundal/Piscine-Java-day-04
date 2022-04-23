7a9ac6fe57263361a8a9be3beefbba854df306da
ferundal <ferundal@gmail.com>
2022-04-23 17:16:22 +0400

    ex01 with changed program

--- ex01/ImagesToChar/src/java/edu/school21/printer/app/Program.java
+++ ex01/ImagesToChar/src/java/edu/school21/printer/app/Program.java
@@ -6,6 +6,7 @@ import edu.school21.printer.logic.Painter;
 import edu.school21.printer.logic.WrongImageFileFormat;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
 
 public class Program {
     public static void main(String [] args) {
@@ -16,16 +17,16 @@ public class Program {
                 throw new WrongCommandLineArguments();
             }
             try {
-                bufferedImage = ImageIO.read(new File("/resources/it.bmp"));
+                bufferedImage = ImageIO.read(Program.class.getResource("/resources/it.bmp"));
             } catch (IOException ioException) {
-                throw new WrongCommandLineArguments();
+                throw new RuntimeException();
             }
         } catch (WrongCommandLineArguments wrongCommandLineArguments) {
             System.err.println(wrongCommandLineArguments.toString());
             System.exit(-1);
         }
         try {
-            new Painter(bufferedImage, args[1].toCharArray()[0], args[2].toCharArray()[0]).paint();
+            new Painter(bufferedImage, args[0].toCharArray()[0], args[1].toCharArray()[0]).paint();
         } catch (WrongImageFileFormat wrongImageFileFormat) {
             System.err.println(wrongImageFileFormat.toString());
         }
